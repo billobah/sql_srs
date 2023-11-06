4d759e9d4ab16cf192005b4d68fa6752707e341a
Billo BAH <billobah@yahoo.fr>
2023-11-06 15:57:19 +0100

    comparison with real answer

--- app.py
+++ app.py
@@ -7,10 +7,7 @@ import duckdb
 import pandas as pd
 import streamlit as st
 
-
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
-
-# solution_df = duckdb.sql(ANSWER_STR).df()
 
 with st.sidebar:
     theme = st.selectbox(
@@ -24,25 +21,30 @@ with st.sidebar:
     exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
     st.write(exercise)
 
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+
+        solution_df = con.execute(answer).df()
 
 st.header("Enter your code:")
 query = st.text_area(label="Votre code SQL ici", key="user_input")
-# if query:
-#     result = con.execute(query).df()
-#     st.dataframe(result)
-#
-#     try:
-#         result = result[solution_df.columns]
-#         st.dataframe(result.compare(solution_df))
-#     except KeyError as e:
-#         st.write("Some columns are missing")
-#
-#     n_lines_difference = result.shape[0] - solution_df.shape[0]
-#     if n_lines_difference != 0:
-#         st.write(
-#             f"result has a {n_lines_difference} lines difference with the solution_df"
-#         )
-#
+if query:
+    result = con.execute(query).df()
+    st.dataframe(result)
+
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+    except KeyError as e:
+        st.write("Some columns are missing")
+
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
+        st.write(
+            f"result has a {n_lines_difference} lines difference with the solution_df"
+        )
+
 
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
@@ -54,7 +56,4 @@ with tab2:
         st.dataframe(df_table)
 
 with tab3:
-    exercise_name = exercise.loc[0, "exercise_name"]
-    with open(f"answers/{exercise_name}.sql", "r") as f:
-        answer = f.read()
     st.write(answer)
